751438776d7e42b2756de7a3a02beea5657a8ffd
Lotfullah <andishmandlotfullah21@gmail.com>
2024-09-01 18:26:58 +0530

    name changed

--- 23-react-gallery-unsplash-api/src/utils/utils.js
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-
-const customFetchUrl = axios.create({
-  baseURL:
-    "https://api.unsplash.com/search/photos?client_id=yr73Fo2q2EmQ-bT6107Sbq6FR5rqWJyuDNl7xGmiReE&query=office",
-});
-
-export default customFetchUrl;
